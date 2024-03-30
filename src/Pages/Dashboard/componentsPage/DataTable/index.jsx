8b017a9d7a45644a6009ba22340df911624395e6
ErickCastroo <castrocamachoerickmiguel21@gmail.com>
2024-03-30 04:30:45 -0700

    Refactor EstadisticasUsuarios component to fetch admins and trainers count from backend

--- src/Pages/Dashboard/componentsPage/DataTable/index.jsx
+++ src/Pages/Dashboard/componentsPage/DataTable/index.jsx
@@ -155,3 +155,196 @@ function DataTable() {
 }
 
 export { DataTable };
+
+
+
+
+
+
+
+
+
+
+
+// pendiente de backend para obtener los datos de los usuarios, tengo que poner muchos datos en los usuarios para que se vea bien
+
+// import React, { useState, useEffect } from "react";
+// import { back_url } from "@/config/const";
+// import { useAuth } from "@/contexts/AuthContext/useAuth";
+// import {
+//   useReactTable,
+//   getCoreRowModel,
+//   flexRender,
+//   getPaginationRowModel,
+//   getSortedRowModel,
+//   getFilteredRowModel,
+// } from "@tanstack/react-table";
+// import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
+// import { MdFirstPage, MdLastPage } from "react-icons/md";
+
+// function DataTable() {
+//   const { usuario } = useAuth();
+//   const [usuarios, setUsuarios] = useState([]);
+//   const [sorting, setSorting] = useState([]);
+//   const [filter, setFilter] = useState("");
+
+
+//   useEffect(() => {
+//     const fetchUsuarios = async () => {
+//       try {
+//         const response = await fetch(`${back_url}/usuarios`, {
+//           headers: {
+//             Authorization: `Bearer ${usuario.token}`,
+//           },
+//         });
+//         if (response.ok) {
+//           const usuariosData = await response.json();
+//           setUsuarios(usuariosData);
+//         } else {
+//           console.error("Error al obtener los datos de usuarios");
+//         }
+//       } catch (error) {
+//         console.error("Error en la petición fetch para obtener los datos de usuarios", error);
+//       }
+//     };
+
+//     if (usuario) {
+//       fetchUsuarios();
+//     }
+//   }, []);
+
+//   const columnas = [
+//     {
+//       header: "Matricula",
+//       accessorKey: "matricula",
+//     },
+//     {
+//       header: "Nombre",
+//       accessorKey: "nombre", 
+//     },
+//     {
+//       header: "Correo",
+//       accessorKey: "correo", 
+//       footer: "mi correo",
+//     },
+//     {
+//       header: "Rol",
+//       accessorKey: "rol",
+//       footer: "mi rol",
+//     },
+//   ];
+
+//   const table = useReactTable({
+//     data: usuarios,
+//     columns: columnas,
+//     getCoreRowModel: getCoreRowModel(),
+//     getPaginationRowModel: getPaginationRowModel(),
+//     getSortedRowModel: getSortedRowModel(),
+//     getFilteredRowModel: getFilteredRowModel(),
+//     state: {
+//       sorting,
+//       globalFilter: filter,
+//     },
+//     onSortingChange: setSorting,
+//     onGlobalFilterChange: setFilter,
+//   });
+
+//   return (
+//     <div className="w-3/4 p-4  bg-white dark:bg-slate-900 dark:border border-gray-200 dark:border-gray-200 rounded-md shadow-lg">
+//       {" "}
+//       {/* Aplicar el fondo, sombra, ancho y relleno a toda la sección */}
+//       <div className="form">
+//         <input
+//           className="dark:text-slate-200 dark:bg-slate-900 dark:placeholder-slate-400 border border-gray-400 p-2 rounded-md flex-grow"
+//           placeholder="Buscar"
+//           type="text"
+//           value={filter}
+//           onChange={(e) => setFilter(e.target.value)}
+//         />
+//       </div>
+//       <table className=" m-6 mx-auto w-full">
+//         <thead className=" dark:bg-slate-900 border-gray-300 dark:border-slate-900">
+//           {table.getHeaderGroups().map((headerGroup) => (
+//             <tr key={headerGroup.id}>
+//               {headerGroup.headers.map((header) => (
+//                 <th
+//                   key={header.id}
+//                   onClick={() => header.column.getToggleSort()}
+//                 >
+//                   {header.isPlaceholder ? null : (
+//                     <div>
+//                       {flexRender(
+//                         header.column.columnDef.header,
+//                         header.getContext()
+//                       )}
+//                     </div>
+//                   )}
+//                 </th>
+//               ))}
+//             </tr>
+//           ))}
+//         </thead>
+
+//         <tbody className="divide-y divide-gray-200 dark:divide-slate-700">
+//           {table.getRowModel().rows.map((row) => (
+//             <tr
+//               key={row.id}
+//               className="hover:bg-gray-100 dark:hover:bg-slate-800"
+//             >
+//               {row.getVisibleCells().map((cell) => (
+//                 <td key={cell.id} className="py-1 px-1">
+//                   {flexRender(cell.column.columnDef.cell, cell.getContext())}
+//                 </td>
+//               ))}
+//             </tr>
+//           ))}
+//         </tbody>
+//       </table>
+//       <div className="mt-3 flex items-center space-x-2">
+//         <button
+//           onClick={() => table.setPageIndex(0)}
+//           className="pagination-button"
+//         >
+//           <MdFirstPage className="pagination-icon" />
+//         </button>
+//         <button
+//           onClick={() => table.previousPage()}
+//           className="pagination-button"
+//         >
+//           <FaArrowLeft className="pagination-icon" />
+//         </button>
+//         <span className="text-gray-600 dark:text-slate-200">
+//           Página {table.options.state.pagination.pageIndex + 1} de{" "}
+//           {Math.max(1, table.getPageCount())}
+//         </span>
+//         <button
+//           onClick={() => {
+//             if (
+//               table.options.state.pagination.pageIndex <
+//               table.getPageCount() - 1
+//             ) {
+//               table.nextPage();
+//             }
+//           }}
+//           className="pagination-button"
+//           disabled={
+//             table.options.state.pagination.pageIndex ===
+//             table.getPageCount() - 1
+//           }
+//         >
+//           <FaArrowRight className="pagination-icon" />
+//         </button>
+//         <button
+//           onClick={() =>
+//             table.setPageIndex(Math.max(0, table.getPageCount() - 1))
+//           }
+//           className="pagination-button"
+//         >
+//           <MdLastPage className="pagination-icon" />
+//         </button>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export { DataTable };
--- src/Pages/Dashboard/componentsPage/estadisticasUsuarios/index.jsx
+++ src/Pages/Dashboard/componentsPage/estadisticasUsuarios/index.jsx
@@ -49,3 +49,105 @@ function BoxWrapper({ iconColor, icon, title, value, change }) {
   );
 }
 export { EstadisticasUsuarios }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// pendiente de backend para obtener la cantidad de admins y entrenadores solo que tenemos que modificar el backend para que nos devuelva la cantidad de admins y entrenadores
+
+
+// import React, { useState, useEffect } from 'react';
+// import { FaRobot } from 'react-icons/fa';
+// import { RiAdminLine } from 'react-icons/ri';
+// import { GrUserAdmin } from 'react-icons/gr';
+// import { useAuth } from '@/contexts/AuthContext/useAuth';
+
+// function EstadisticasUsuarios() {
+//   const { usuario } = useAuth();
+//   const [adminsCount, setAdminsCount] = useState(0);
+//   const [trainersCount, setTrainersCount] = useState(0);
+
+//   useEffect(() => {
+//     fetchUserData();
+//   }, []);
+
+//   const fetchUserData = async () => {
+//     try {
+//       const response = await fetch(`${back_url}/usuarios`, {
+//         headers: {
+//           Authorization: `Bearer ${usuario.token}`,
+//         },
+//       });
+//       if (response.ok) {
+//         const userData = await response.json();
+//         const admins = userData.filter((user) => user.rol === 1);
+//         const trainers = userData.filter((user) => user.rol === 0); 
+//         setAdminsCount(admins.length);
+//         setTrainersCount(trainers.length);
+//       } else {
+//         console.error('Error al obtener los datos de usuarios');
+//       }
+//     } catch (error) {
+//       console.error('Error en la petición fetch para obtener los datos de usuarios', error);
+//     }
+//   };
+
+//   return (
+//     <div className="flex flex-col md:flex-row gap-4">
+//       <BoxWrapper
+//         iconColor="bg-sky-500"
+//         icon={<RiAdminLine className="text-2xl text-white" />}
+//         title="Total de admin"
+//         value={adminsCount}
+//         change=""
+//       />
+//       <BoxWrapper
+//         iconColor="bg-orange-600"
+//         icon={<GrUserAdmin className="text-2xl text-white" />}
+//         title="Total entrenadores"
+//         value={trainersCount}
+//         change=""
+//       />
+//       <BoxWrapper
+//         iconColor="bg-green-600"
+//         icon={<FaRobot className="text-2xl text-white" />}
+//         title="Estado del Chat-Bot"
+//         value="entrenando"
+//         change=""
+//       />
+//     </div>
+//   );
+// }
+
+// function BoxWrapper({ iconColor, icon, title, value, change }) {
+//   return (
+//     <div className="bg-white dark:bg-slate-900 rounded-sm p-4 flex-1 border border-gray-200 flex items-center">
+//       <div className={`rounded-full md:h-12 md:w-12 h-10 w-10 flex items-center justify-center ${iconColor}`}>
+//         {icon}
+//       </div>
+//       <div className="pl-4">
+//         <span className="text-sm dark:text-slate-200 text-gray-500 font-light">{title}</span>
+//         <div className="flex items-center">
+//           <strong className="text-xl dark:text-slate-200 text-gray-700 font-semibold">{value}</strong>
+//           <span className={`text-sm ${change.startsWith('-') ? 'text-red-500' : 'text-green-500'} pl-2`}>{change}</span>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export { EstadisticasUsuarios };
